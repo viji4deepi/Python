6e42a5ae3baa6d29ea218b7450831d1ee8799e12
unknown <viji4deepi@gmail.com>
2023-04-05 17:06:05 -0500

    print program

---
+++ Basics/print.py
@@ -0,0 +1,2 @@
+# This is a print program
+print("Hello World!")
---
+++ Basics/variables.py
@@ -0,0 +1,20 @@
+x = 5
+y = "John"
+z = 'Tommy'
+
+print(x)
+print(y)
+print(z)
+
+
+# data tyoe with casting
+a= str(3)    # a will be '3'
+b = int(3)    # b will be 3
+c = float(3)  # c will be 3.0
+
+print(a, b, c)
+
+
+# find variable with the type() function.
+print(type(x))
+print(type(y))
